9a28d98b850c72c34b4a6510e3f96daf8aa05404
granici-web-dev <granici.design@gmail.com>
2019-09-25 13:08:51 +0200

    added new items-page

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ gulp.task('style', function(){
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -36,6 +37,7 @@ gulp.task('script', function(){
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
