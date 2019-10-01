6f96c244eab68462e184b69b3bf32a0841d827b2
granici-web-dev <granici.design@gmail.com>
2019-10-01 17:02:57 +0200

    added new page product-one

--- app/js/main.js
+++ app/js/main.js
@@ -41,6 +41,15 @@ $(function(){
       $('.header__box').toggleClass('active');
     });
 
+    $('.product-one__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+      });
+
    
     
 
