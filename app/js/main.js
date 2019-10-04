c6a0d7fcda2cc72ded4d2c11515b5571cd6e29a1
granici-web-dev <granici.design@gmail.com>
2019-10-04 13:41:39 +0200

    added settings page

--- app/js/main.js
+++ app/js/main.js
@@ -41,10 +41,10 @@ $(function(){
       $('.header__box').toggleClass('active');
     });
 
-    $('.product-one__tabs .tab').on('click', function(event) {
+    $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
       var id = $(this).attr('data-id');
-        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
         $(this).addClass('active');
         $('#'+id).addClass('active-tab').fadeIn();
         return false;
