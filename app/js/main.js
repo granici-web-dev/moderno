c8fe9a3431b3e74d54c25261b5d3ca6044780411
granici-web-dev <granici.design@gmail.com>
2019-10-14 14:27:26 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,31 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1900,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 1441,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 801,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+        ]
       });
 
       $(".js-range-slider").ionRangeSlider({
